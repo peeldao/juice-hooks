a32efbecb85b742a67a2e3ba883b7c5c64df0c17
Wraeth <wraeth.dao@gmail.com>
2023-09-26 16:10:30 +1000

    utils-data-docs

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,4 +1,5 @@
 // JB-specific below
+// ^ 1) what
 import { FixedInt, FixedPortion } from "fpnum";
 import {
   JB_TOKEN_DECIMALS,
@@ -7,36 +8,78 @@ import {
   MAX_RESERVED_RATE,
 } from "../constants";
 
+/**
+ * Reserved rate for a funding cycle.
+ *
+ * Has a decimal precision of 4 and a maximum value of 10,000.
+ *
+ * @extends FixedPortion
+ */
 export class ReservedRate extends FixedPortion<4> {
   constructor(value: bigint) {
     super(value, 4, MAX_RESERVED_RATE);
   }
 }
 
+/**
+ * Redemption rate for a funding cycle.
+ *
+ * Has a decimal precision of 4 and a maximum value of 10,000.
+
+ * @extends FixedPortion
+ */
 export class RedemptionRate extends FixedPortion<4> {
   constructor(value: bigint) {
     super(value, 4, MAX_REDEMPTION_RATE);
   }
 }
 
+/**
+ * Discount rate for a funding cycle.
+ *
+ * Has a decimal precision of 9 and a maximum value of 1,000,000,000.
+
+ * @extends FixedPortion
+ */
 export class DiscountRate extends FixedPortion<9> {
   constructor(value: bigint) {
     super(value, 9, MAX_DISCOUNT_RATE);
   }
 }
 
+/**
+ * Ether value.
+ *
+ * Has a decimal precision of 18.
+ *
+ * @extends FixedInt
+ */
 export class Ether extends FixedInt<18> {
   constructor(value: bigint) {
     super(value, 18);
   }
 }
 
+/**
+ * JB token value.
+ *
+ * Has a decimal precision of 18.
+ *
+ * @extends FixedInt
+ */
 export class JBToken extends FixedInt<typeof JB_TOKEN_DECIMALS> {
   constructor(value: bigint) {
     super(value, JB_TOKEN_DECIMALS);
   }
 }
 
+/**
+ * Funding cycle weight.
+ *
+ * Has a decimal precision of 18.
+ *
+ * @extends FixedInt
+ */
 export class FundingCycleWeight extends FixedInt<18> {
   constructor(value: bigint) {
     super(value, 18);
