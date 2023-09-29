48e4a0ab644b81e8a0cbfb1344c64afe29560327
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-09-29 10:47:52 +0200

    try bundling again

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  splitting: true,
+  sourcemap: true,
+  bundle: true,
+  clean: true,
+  target: "es2021",
+});
