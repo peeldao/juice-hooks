127c3991008099340094faa4570ddbc110329b0a
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-11-06 06:49:25 +1000

    touchups

--- src/react/hooks/useFormatEthAddress.ts
+++ src/react/hooks/useFormatEthAddress.ts
@@ -4,6 +4,8 @@ import { Address, useEnsName } from "wagmi";
 import { AsyncData } from "../contexts";
 
 /**
+ * TODO USE HOOK FROM JUICEHOOKS INSTEAD
+ *
  * Return a formatted representation of an Ethereum address.
  * If the address resolves to an ENS name, it will be returned instead.
  *
@@ -12,15 +14,17 @@ import { AsyncData } from "../contexts";
  * @param opts.truncateTo If the return value is a full-length Address (not an ENS name), truncate the Address to the given number of characters.
  */
 export function useFormatEthAddress(
-  address: Address,
+  address: Address | undefined,
   opts?: { disableEns?: boolean; truncateTo?: number }
-): AsyncData<string> {
+): AsyncData<string | null> {
   const { data: ensName, isLoading } = useEnsName({
     address: !opts?.disableEns ? address : undefined,
     enabled: !opts?.disableEns,
   });
 
   const formattedAddress = useMemo(() => {
+    if (!address) return null;
+
     if (!opts?.disableEns && ensName) {
       return ensName;
     }
