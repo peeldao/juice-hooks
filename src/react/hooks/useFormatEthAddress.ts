817366478c346d39b56b5f5412d4b7be0563ff12
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-11-06 06:42:45 +1000

    touchups

--- src/react/hooks/useFormatEthAddress.ts
+++ src/react/hooks/useFormatEthAddress.ts
@@ -1,9 +1,11 @@
+import { useMemo } from "react";
 import { formatEthAddress } from "src/utils";
 import { Address, useEnsName } from "wagmi";
+import { AsyncData } from "../contexts";
 
 /**
  * Return a formatted representation of an Ethereum address.
- * If an ENS name is available, it will be returned instead.
+ * If the address resolves to an ENS name, it will be returned instead.
  *
  * @param address The address to format
  * @param opts.disableEns If true, the ENS name will not be fetched or returned.
@@ -12,15 +14,22 @@ import { Address, useEnsName } from "wagmi";
 export function useFormatEthAddress(
   address: Address,
   opts?: { disableEns?: boolean; truncateTo?: number }
-): string {
-  const { data: ensName } = useEnsName({
+): AsyncData<string> {
+  const { data: ensName, isLoading } = useEnsName({
     address: !opts?.disableEns ? address : undefined,
     enabled: !opts?.disableEns,
   });
 
-  if (!opts?.disableEns && ensName) {
-    return ensName;
-  }
+  const formattedAddress = useMemo(() => {
+    if (!opts?.disableEns && ensName) {
+      return ensName;
+    }
 
-  return formatEthAddress(address, { truncateTo: opts?.truncateTo });
+    return formatEthAddress(address, { truncateTo: opts?.truncateTo });
+  }, [address, opts?.truncateTo]);
+
+  return {
+    isLoading,
+    data: formattedAddress,
+  };
 }
