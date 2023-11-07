be12de322c8def4e39e0fb3fbfcf1e69f096decc
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-11-07 20:13:22 +1000

    useFormattedEthAddress

--- src/react/hooks/useFormattedEthAddress.ts
+++ src/react/hooks/useFormattedEthAddress.ts
@@ -1,36 +1,36 @@
-import { useMemo } from "react";
 import { formatEthAddress } from "src/utils";
+import { useMemo } from "react";
 import { Address, useEnsName } from "wagmi";
-import { AsyncData } from "../contexts";
 
 /**
- * TODO USE HOOK FROM JUICEHOOKS INSTEAD
- *
  * Return a formatted representation of an Ethereum address.
  * If the address resolves to an ENS name, it will be returned instead.
  *
  * @param address The address to format
- * @param opts.disableEns If true, the ENS name will not be fetched or returned.
+ * @param [opts.enableEns=true] try to resolve [address] to an ENS name. If found, return it. Defaults to true.
  * @param opts.truncateTo If the return value is a full-length Address (not an ENS name), truncate the Address to the given number of characters.
  */
-export function useFormatEthAddress(
+export function useFormattedEthAddress(
   address: Address | undefined,
-  opts?: { disableEns?: boolean; truncateTo?: number }
-): AsyncData<string | null> {
+  {
+    enableEns = true,
+    truncateTo,
+  }: { enableEns: boolean; truncateTo?: number } = { enableEns: true }
+): { isLoading: boolean; data: string | null } {
   const { data: ensName, isLoading } = useEnsName({
-    address: !opts?.disableEns ? address : undefined,
-    enabled: !opts?.disableEns,
+    address: enableEns ? address : undefined,
+    enabled: enableEns,
   });
 
   const formattedAddress = useMemo(() => {
     if (!address) return null;
 
-    if (!opts?.disableEns && ensName) {
+    if (enableEns && ensName) {
       return ensName;
     }
 
-    return formatEthAddress(address, { truncateTo: opts?.truncateTo });
-  }, [address, opts, ensName]);
+    return formatEthAddress(address, { truncateTo });
+  }, [address, enableEns, truncateTo, ensName]);
 
   return {
     isLoading,
--- src/react/index.ts
+++ src/react/index.ts
@@ -2,4 +2,4 @@ export * from "./contexts";
 export * from "./generated/hooks";
 export * from "./hooks/jb721Delegate/useJb721DelegateTiers";
 export * from "./hooks/juicebox/pay/usePayEthPaymentTerminal";
-export * from "./hooks/useFormatEthAddress";
+export * from "./hooks/useFormattedEthAddress";
