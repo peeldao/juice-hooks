1287747552d671bce1e23545ca4ec2d75078bbdd
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-11-08 20:16:56 +1000

    poop

--- src/react/hooks/jb721Delegate/useJb721DelegateVersion.ts
+++ src/react/hooks/jb721Delegate/useJb721DelegateVersion.ts
@@ -1,21 +1,26 @@
 import { goerli } from "viem/chains";
 import { Address, useQuery } from "wagmi";
 import { useChain } from "../useNetwork";
+import { isAddressEqual, zeroAddress } from "viem";
 
 export function useJb721DelegateVersion(datasource: Address | undefined) {
   const chain = useChain();
 
   return useQuery(["jb721DelegateVersion", datasource], async () => {
-    if (!datasource) {
+    if (!datasource || isAddressEqual(datasource, zeroAddress)) {
       return null;
     }
 
-    const prefix = chain.id === goerli.id ? "goerli." : "";
-    const response = await fetch(
-      `https://${prefix}juicebox.money/api/juicebox/jb-721-delegate/${datasource}`
-    );
-    const data = (await response.json()) as { version: string };
+    try {
+      const prefix = chain.id === goerli.id ? "goerli." : "";
+      const response = await fetch(
+        `https://${prefix}juicebox.money/api/juicebox/jb-721-delegate/${datasource}`
+      );
 
-    return data.version;
+      const data = (await response.json()) as { version: string | undefined };
+      return data.version ?? null;
+    } catch (e) {
+      return null;
+    }
   });
 }
