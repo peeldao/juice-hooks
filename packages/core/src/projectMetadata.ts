7ded5a3a5896ad341bf8e817008971fe819eebba
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-12-14 09:55:57 +1000

    smoke bongs for life

---
+++ packages/core/src/projectMetadata.ts
@@ -0,0 +1,48 @@
+import { jbProjectsABI, jbProjectsAddress } from "./generated/core";
+import { JBProjectMetadata } from "./types";
+import { ipfsGatewayUrl } from "./utils/ipfs";
+import { PublicClient, getContract } from "viem";
+
+type JBChainId = 1 | 5;
+
+const getMetadataCid = async (
+  publicClient: PublicClient,
+  args: {
+    projectId: bigint;
+    domain: bigint;
+  }
+) => {
+  const chainId = (await publicClient.getChainId()) as JBChainId;
+  const JBProjects = await getContract({
+    address: jbProjectsAddress[chainId],
+    abi: jbProjectsABI,
+    publicClient,
+  });
+
+  const metadataCid = (await JBProjects.read.metadataContentOf([
+    args.projectId,
+    args.domain,
+  ])) as string;
+
+  return metadataCid;
+};
+
+export const getProjectMetadata = async (
+  publicClient: PublicClient,
+  args: {
+    projectId: bigint;
+    domain: bigint;
+  },
+  opts?: {
+    ipfsGatewayHostname?: string;
+  }
+): Promise<JBProjectMetadata | undefined> => {
+  const metadataCid = await getMetadataCid(publicClient, args);
+  const ipfsUrl = ipfsGatewayUrl(metadataCid, opts?.ipfsGatewayHostname);
+  // TODO: Use juiceFetch here.
+  const res = (await fetch(ipfsUrl).then((res) => res.json())) as
+    | JBProjectMetadata
+    | undefined;
+
+  return res;
+};
---
+++ packages/react/src/contexts/JBTokenContext/JBTokenContext.tsx
@@ -0,0 +1,102 @@
+import React, { PropsWithChildren, createContext, useContext } from "react";
+import { useToken } from "wagmi";
+import {
+  useJbController3_1TotalOutstandingTokensOf,
+  useJbTokenStoreTokenOf,
+} from "../../generated/hooks";
+import { useJBContractContext } from "../JBContractContext/JBContractContext";
+import { AsyncData, AsyncDataNone } from "../types";
+import { JBToken } from "juice-sdk-core/utils/data";
+import { FetchTokenResult } from "wagmi/dist/actions";
+import { isAddressEqual, zeroAddress } from "viem";
+
+/**
+ * Context for the token of a project.
+ */
+export type JBTokenContextData = {
+  /**
+   * The token of the project.
+   */
+  token: AsyncData<FetchTokenResult>;
+  /**
+   * The total outstanding tokens of the project.
+   */
+  totalOutstanding: AsyncData<JBToken>;
+};
+
+/**
+ * Context for the token of a project.
+ */
+export const JBTokenContext = createContext<JBTokenContextData>({
+  /**
+   * The token of the project.
+   *
+   * @default none {@link AsyncDataNone}
+   */
+  token: AsyncDataNone,
+  /**
+   * The total outstanding tokens of the project.
+   *
+   * @default none {@link AsyncDataNone}
+   */
+  totalOutstanding: AsyncDataNone,
+});
+
+export function useJBTokenContext() {
+  return useContext(JBTokenContext);
+}
+
+export type JBTokenProviderProps = PropsWithChildren<{
+  projectId: bigint;
+  withTotalOutstanding?: boolean;
+}>;
+
+/**
+ * Provides the token for a project.
+ *
+ * @note depends on JBContractContext
+ */
+export const JBTokenProvider = ({
+  projectId,
+  children,
+  withTotalOutstanding,
+}: JBTokenProviderProps) => {
+  const {
+    contracts: { controller },
+  } = useJBContractContext();
+
+  const { data: tokenAddress } = useJbTokenStoreTokenOf({
+    args: [projectId],
+  });
+  const fetchTokenEnabled = Boolean(
+    tokenAddress && !isAddressEqual(tokenAddress, zeroAddress)
+  );
+  const token = useToken({
+    address: fetchTokenEnabled ? tokenAddress : undefined,
+    enabled: fetchTokenEnabled,
+  });
+
+  const totalOutstandingRes = useJbController3_1TotalOutstandingTokensOf({
+    address: controller?.data,
+    args: withTotalOutstanding ? [projectId] : undefined,
+    enabled: withTotalOutstanding && controller?.data !== undefined,
+  });
+
+  const totalOutstandingData = totalOutstandingRes?.data
+    ? new JBToken(totalOutstandingRes?.data)
+    : undefined;
+
+  return (
+    <JBTokenContext.Provider
+      value={{
+        token,
+        totalOutstanding: {
+          data: totalOutstandingData,
+          isLoading: totalOutstandingRes?.isLoading,
+        },
+      }}
+    >
+      {children}
+    </JBTokenContext.Provider>
+  );
+};
